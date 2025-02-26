90a106e60adaef8f467918c8fc99574763ead704
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2025-02-26 14:04:16 +0300

    updates

--- PSec-AI/src/components/Sidebar.jsx
+++ PSec-AI/src/components/Sidebar.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import '../styles/Sidebar.css'; 
-import ReportCard from '../components/ReportCard';
-import { FaSearch } from 'react-icons/fa';
+import React from "react";
+import "../styles/Sidebar.css";
+import ReportCard from "../components/ReportCard";
+import { FaSearch } from "react-icons/fa";
 
 const Sidebar = () => {
+
     return (
         <div className="sidebar-container">
             <div className="sidebar-header">
-            <h2>My Reports</h2>
-            <FaSearch className='search-icon'/>
+                <h2>Recent Reports</h2>
+                <FaSearch className="search-icon" />
             </div>
-            <hr className='sidebar-separater'/>
-            <div className='report-list'>
-                <ReportCard />
-                <ReportCard />
-                <ReportCard />
-                <ReportCard />
-                <ReportCard />
+
+            <div className="report-list">
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
+                    <ReportCard />
             </div>
         </div>
     );
