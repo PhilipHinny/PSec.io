28d92346e8021de5a9235419432612b8257e9518
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2025-03-20 13:49:34 +0300

    updates

--- PSecAI-Server/config.py
+++ PSecAI-Server/config.py
@@ -1,14 +1,14 @@
-# import os
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# OPENAI_API_KEY = os.getenv("API_KEY", "my")
-# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "my api")
-# PINECONE_INDEX_NAME = "reports"
-# DB_CONFIG = {
-#     "dbname": os.getenv("DB_NAME"),
-#     "user": os.getenv("DB_USER"),
-#     "password": os.getenv("DB_PASSWORD"),
-#     "host": os.getenv("DB_HOST"),
-# }
+OPENAI_API_KEY = os.getenv(OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","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"))
+PINECONE_API_KEY = os.getenv(PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "pcsk_2EcWqJ_UC24u6QGzgr5FAxzgKcVui8aD91HLp5a1G198ri8aSgdAvcuqQFtE26U29k34SP"))
+PINECONE_INDEX_NAME = "reports"
+DB_CONFIG = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+}
