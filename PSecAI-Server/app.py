bcb7059741ce02e39bae541acc3e7ce3292aa835
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2025-03-31 18:18:03 +0300

    updates

--- PSecAI-Server/app.py
+++ PSecAI-Server/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from routes.upload_routes import upload_bp
 from routes.generate_routes import generate_bp
 from routes.get_reports_routes import get_reports_bp
+from routes.sidebar import report_bp
 
 app = Flask(__name__)
 
@@ -13,6 +14,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(generate_bp)
 app.register_blueprint(upload_bp)
 app.register_blueprint(get_reports_bp)
+app.register_blueprint(report_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
