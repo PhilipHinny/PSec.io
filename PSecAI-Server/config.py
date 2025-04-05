4bd7d88c23ba308163c1be89b04d5eed12e4ce27
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2025-04-05 20:02:57 +0300

    Update on Database

--- PSecAI-Server/config.py
+++ PSecAI-Server/config.py
@@ -8,6 +8,6 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "PINECONE_API")
 PINECONE_INDEX_NAME = "reports"
 
 DB_CONFIG = {
-    "MONGO_URI": os.getenv("MONGO_URI", "mongodb://localhost:27017/"),
+    "MONGO_URI": os.getenv("MONGO_URI", "mongodb+srv://PsecAI:Kasy%40123@cluster0.1vsiqgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),
     "DB_NAME": os.getenv("DB_NAME", "reports_db")
 }
