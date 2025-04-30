2a6f62d81c482ca21d1d71062eaf50382ba5fb34
Philip Hinny <115541979+PhilipHinny@users.noreply.github.com>
2025-04-30 16:59:45 +0300

    Update app.py hosting update

--- PSecAI-Server/app.py
+++ PSecAI-Server/app.py
@@ -27,4 +27,5 @@ app.register_blueprint(Download_bp, name='download_report')
 app.register_blueprint(Dashboardupload_bp, name='Dashboard_Upload_report')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
