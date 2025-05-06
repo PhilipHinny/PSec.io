eb631bd25d4084fbe940975379c67d7658d77530
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2025-05-06 19:36:04 +0300

    updates

--- PSecAI-Server/routes/stripepayement.py
+++ PSecAI-Server/routes/stripepayement.py
@@ -1,8 +1,13 @@
+import os
 import stripe
 from flask import Blueprint, jsonify, request
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Initialize Stripe with your secret key
-stripe.api_key = "pk_test_51RLo67IJlJJgM2f2Jl2jA4PklfwnZEbWKl9tSXHwZryi9G1DiARNIM5tYD3vK8rgQcKJB1WoaPy5gZ4RosspNIPV00vpwsDFbq"
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
 # Create a Blueprint for Stripe routes
 stripePayment_bp = Blueprint('stripe_payment', __name__)
@@ -23,6 +28,6 @@ def create_payment_intent():
         return jsonify({
             'clientSecret': payment_intent.client_secret
         })
-    
+
     except Exception as e:
         return jsonify(error=str(e)), 403
