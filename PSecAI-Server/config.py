fa8adbed125346341d01a7ad3074aabecd88539e
Philip Hinny <hinnyp5@gmail.com>
2025-09-03 00:58:10 +0300

    BACK END

--- PSecAI-Server/config.py
+++ PSecAI-Server/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 
 DB_CONFIG = {
-    "MONGO_URI": os.getenv("MONGO_URI", "mongodb+srv://PsecAI:Kasy%40123@cluster0.1vsiqgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),
-    "DB_NAME": os.getenv("DB_NAME", "PSecAI_db")
+    "MONGO_URI": os.getenv("MONGO_URI", "mongodb+srv://PSECAI:Kasy%40123@cluster0.1vsiqgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),
+    "DB_NAME": os.getenv("DB_NAME", "PSECAI_db")
 }
